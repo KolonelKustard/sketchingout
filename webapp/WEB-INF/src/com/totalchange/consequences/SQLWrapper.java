9bc837dbca16de4c6c9ce250ea92e7bbcdb6d37b
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-05-11 22:23:37 +0000

    A Java class that provides a layer of abstraction for Consequences access to the database.  Means can drop in altered/optimised db code without changing the core app at all.

---
+++ webapp/WEB-INF/src/com/totalchange/consequences/SQLWrapper.java
@@ -0,0 +1,45 @@
+/*
+ * Created on 05-May-2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.totalchange.consequences;
+
+import java.sql.*;
+
+/**
+ * @author Ralph Jones
+ *
+ * <p>This class encapsulates all the SQL queries used by the consequences
+ * application.  The aim is to allow easier transport from one RDBMS to
+ * another.</p>
+ * <p>The reason an object relational mapping tool like Hibernate hasn't been
+ * used is purely because of portability.  We cannot presently afford to pay
+ * for decent hosting, so must work towards the lowest spec.  Thus explaining
+ * why this current version is using HSQLDB</p>
+ */
+public class SQLWrapper {
+	private static final String DB_CLASSNAME = "org.hsqldb.jdbcDriver";
+	private static final String DB_URL = "";
+	private static final String DB_USERNAME = "";
+	private static final String DB_PASSWORD = "";
+	
+	public static Connection makeConnection() throws ClassNotFoundException, 
+	  SQLException {
+		Class.forName(DB_CLASSNAME);
+		Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+		return conn;
+	}
+	
+	public static ResultSet getUser(Connection conn, String userID) throws
+	  SQLException {
+		PreparedStatement pstmt = conn.prepareStatement(
+		  "SELECT * FROM users WHERE user_id = ?"
+		);
+		
+		pstmt.setString(1, userID);
+		
+		return pstmt.executeQuery();
+	}
+}
