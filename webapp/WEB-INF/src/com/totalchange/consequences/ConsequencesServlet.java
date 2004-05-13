5d9f2f3fa608d196db159c4e8d51962d33ced13b
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-05-13 10:58:39 +0000

    Now calls SAX XML Parser

--- webapp/WEB-INF/src/com/totalchange/consequences/ConsequencesServlet.java
+++ webapp/WEB-INF/src/com/totalchange/consequences/ConsequencesServlet.java
@@ -4,12 +4,17 @@
  */
 package com.totalchange.consequences;
 
+import com.totalchange.consequences.XMLHandler;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 /**
  * @author Ralph Jones
@@ -20,13 +25,22 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ConsequencesServlet extends HttpServlet {
 
-	/* (non-Javadoc)
+	/* 
+	 * <p>Handles raw post data as xml and passes it to the SAX parser to decide
+	 * on the appropriate response</p>
+	 * 
 	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
 		
-		
+		try {
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser parser = factory.newSAXParser();
+			parser.parse(request.getInputStream(), new XMLHandler(response.getOutputStream()));
+		}
+		catch(Exception e) {
+		}
 	}
 
 }
