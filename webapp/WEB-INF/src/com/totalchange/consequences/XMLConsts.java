414960f403c05912941168af175f7f83af9eda72
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-05-31 17:50:05 +0000

    The names of the XML elements are stored here.

---
+++ webapp/WEB-INF/src/com/totalchange/consequences/XMLConsts.java
@@ -0,0 +1,23 @@
+/*
+ * Created on 31-May-2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.totalchange.consequences;
+
+/**
+ * @author RalphJones
+ *
+ * <p>Pointless class that provides String constants for the various XML
+ * elements and attributes or whatever.</p>
+ */
+public class XMLConsts {
+	public static final String EL_RESPONSE = "response";
+	
+	public static final String EL_ERRORS = "errors";
+	public static final String EL_ERROR = "error";
+	public static final String EL_ERROR_SRC = "src";
+	public static final String EL_ERROR_TYPE = "type";
+	public static final String EL_ERROR_MESSAGE = "message";
+}
