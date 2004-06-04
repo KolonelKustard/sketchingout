7955114e8dc2949d8c04b8cb22890d736c713615
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-06-04 09:34:05 +0000

    An interface and associated excpetion type for request handlers to implement.  Allows for simple extension of the basic XML request sequence, but allows for quite a low-level stream based system.  Won't be very portable, but should be quite efficient.

---
+++ webapp/WEB-INF/src/com/totalchange/consequences/HandlerException.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 03-Jun-2004
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package com.totalchange.consequences;
+
+/**
+ * @author RalphJones
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class HandlerException extends Exception {
+	public HandlerException(String message) {
+		super(message);
+	}
+}
---
+++ webapp/WEB-INF/src/com/totalchange/consequences/RequestHandler.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 03-Jun-2004
+ *
+ */
+package com.totalchange.consequences;
+
+import java.sql.Connection;
+import org.xml.sax.Attributes;
+
+/**
+ * @author RalphJones
+ *
+ * <p>Interface to allow easier addition of 
+ */
+public interface RequestHandler {
+	public void start(XMLWriter out, Connection conn, ConsequencesErrors errs, 
+		Attributes attributes) throws HandlerException;
+	public void data(char[] ch, int start, int length) throws HandlerException;
+	public RequestHandler getChild(String name) throws HandlerException;
+	public void end() throws HandlerException;
+}
