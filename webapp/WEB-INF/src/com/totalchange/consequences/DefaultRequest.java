419e855986a58254286f2ea1f3de841dd1ec7b68
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-06-04 09:35:28 +0000

    Implements RequestHandler and acts as the root request element.  Just basically a factory responding to requests for sub-handlers.

---
+++ webapp/WEB-INF/src/com/totalchange/consequences/DefaultRequest.java
@@ -0,0 +1,56 @@
+/*
+ * Created on 03-Jun-2004
+ *
+ */
+package com.totalchange.consequences;
+
+import java.sql.Connection;
+
+import org.xml.sax.Attributes;
+
+/**
+ * @author RalphJones
+ *
+ * <p>This class does nothing other than provide a factory service for child
+ * handlers.</p>
+ */
+public class DefaultRequest implements RequestHandler {
+
+	/**
+	 * @see com.totalchange.consequences.RequestHandler#start(java.sql.Connection, com.totalchange.consequences.ConsequencesErrors, org.xml.sax.Attributes)
+	 */
+	public void start(
+		XMLWriter out,
+		Connection conn,
+		ConsequencesErrors errs,
+		Attributes attributes) throws HandlerException {
+			
+	}
+
+	/**
+	 * @see com.totalchange.consequences.RequestHandler#data(char[], int, int)
+	 */
+	public void data(char[] ch, int start, int length) throws HandlerException {
+	}
+
+	/**
+	 * Provides a factory service for request handlers
+	 * 
+	 * @see com.totalchange.consequences.RequestHandler#getChild(java.lang.String)
+	 */
+	public RequestHandler getChild(String name) throws HandlerException {
+		if (name.equals(XMLConsts.EL_USER_DETAILS)) {
+			return new UserRequest();
+		}
+		else {
+			return null;
+		}
+	}
+
+	/**
+	 * @see com.totalchange.consequences.RequestHandler#end()
+	 */
+	public void end() throws HandlerException {
+	}
+
+}
