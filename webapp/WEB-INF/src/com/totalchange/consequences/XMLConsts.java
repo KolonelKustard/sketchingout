f521b394441ab62b49622ad99d8f481dee6066db
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-06-04 09:39:23 +0000

    Added user detail element/attribute names and next drawing element/ attribute names.

--- webapp/WEB-INF/src/com/totalchange/consequences/XMLConsts.java
+++ webapp/WEB-INF/src/com/totalchange/consequences/XMLConsts.java
@@ -23,4 +23,23 @@ public class XMLConsts {
 	public static final String EL_ERROR_MESSAGE = "message";
 	public static final String EL_ERROR_STACK_TRACE = "stacktrace";
 	public static final String EL_ERROR_TRACE = "trace";
+	
+	public static final String EL_USER_DETAILS = "user_details";
+	public static final String AT_USER_ID = "id";
+	public static final String EL_USER_ID = "id";
+	public static final String EL_USER_NAME = "name";
+	public static final String EL_USER_EMAIL = "email";
+	public static final String EL_USER_SIGNATURE = "signature";
+	
+	public static final String EL_NEXT_DRAWING = "next_drawing";
+	public static final String AT_NEXT_DRAWING_ID = "drawing_id";
+	public static final String AT_NEXT_DRAWING_USER_ID = "user_id";
+	public static final String AT_NEXT_DRAWING_BODY_PART = "bodypart";
+		public static final String AV_HEAD = "head";
+		public static final String AV_BODY = "body";
+		public static final String AV_LEGS = "legs";
+		public static final String AV_FEET = "feet";
+	public static final String EL_NEXT_DRAWING_ID = "drawing_id";
+	public static final String EL_NEXT_DRAWING_LOCKED_SECS = "locked_secs";
+	public static final String EL_NEXT_DRAWING_DRAWING = "drawing";
 }
