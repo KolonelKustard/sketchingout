e07741ff82c8411c8fee673992eea6e9b9ed8826
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-06-13 14:50:02 +0000

    A class to contain settings for consequences.

---
+++ webapp/WEB-INF/src/com/totalchange/consequences/ConsequencesSettings.java
@@ -0,0 +1,37 @@
+/*
+ * Created on 13-Jun-2004
+ *
+ */
+package com.totalchange.consequences;
+
+/**
+ * @author RalphJones
+ *
+ * Configuration settings
+ * 
+ */
+public class ConsequencesSettings {
+	/**
+	 * The number of seconds to lock a drawing for once it has been requested.  This
+	 * only applies to publically retrieved drawings.
+	 * 
+	 * 3600 = 60 * 60 (1 hour)
+	 */
+	public static final int DEFAULT_LOCK_SECS = 3600;
+	
+	/**
+	 * The number of seconds to lock a drawing for when it is being passed onto another
+	 * user (i.e. it is a private drawing).
+	 * 
+	 * 86400 = 24 * 60 * 60 (24 hours)
+	 */
+	public static final int PRIVATE_LOCK_SECS = 86400;
+	
+	/**
+	 * This number indicates the maximum number of stages a drawing goes through before
+	 * it's determined to be complete.
+	 * 
+	 * Currently set to 4.  This means 4 stages: "Head", "Body", "Legs", "Feet".
+	 */
+	public static final int MAX_NUM_STAGES = 4;
+}
