55c4aa5769df308084984ad9409e451a6c959c4c
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-06-13 14:49:09 +0000

    Ongoing.  Not yet complete.

--- webapp/WEB-INF/src/com/totalchange/consequences/SubmitDrawingRequest.java
+++ webapp/WEB-INF/src/com/totalchange/consequences/SubmitDrawingRequest.java
@@ -1,24 +1,25 @@
 /*
  * Created on 07-Jun-2004
  *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 package com.totalchange.consequences;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.xml.sax.Attributes;
 
 /**
  * @author RalphJones
  *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 public class SubmitDrawingRequest implements RequestHandler {
+	
+	private PreparedStatement pstmt;
 
-	/* (non-Javadoc)
+	/**
 	 * @see com.totalchange.consequences.RequestHandler#start(com.totalchange.consequences.XMLWriter, java.sql.Connection, com.totalchange.consequences.ConsequencesErrors, org.xml.sax.Attributes)
 	 */
 	public void start(
@@ -27,23 +28,80 @@ public class SubmitDrawingRequest implements RequestHandler {
 		ConsequencesErrors errs,
 		Attributes attributes)
 		throws HandlerException {
+		
+		// Make sure statement is null
+		pstmt = null;
+			
+		// Get required attributes
+		String drawingID = attributes.getValue(XMLConsts.AT_SUBMIT_DRAWING_DRAWING_ID);
+		if (drawingID == null) {
+			throw new HandlerException("Must pass a drawing id to submit a drawing");
+		}
+		
+		String userID = attributes.getValue(XMLConsts.AT_SUBMIT_DRAWING_USER_ID);
+		if (userID == null) {
+			throw new HandlerException("Must pass a user id to submit a drawing");
+		}
+		
+		String stageStr = attributes.getValue(XMLConsts.AT_SUBMIT_DRAWING_STAGE);
+		if (stageStr == null) {
+			throw new HandlerException("Must pass the stage your submitted drawing represents");
+		}
+		
+		String nextUserEmail = attributes.getValue(XMLConsts.AT_SUBMIT_DRAWING_NEXT_USER_EMAIL);
+		
+		// Convert stage to an int.
+		int stage = Integer.valueOf(stageStr).intValue();
+		
+		// Make sure within the bounds
+		if ((stage < 1) || (stage > ConsequencesSettings.MAX_NUM_STAGES)) {
+			throw new HandlerException("Stage value must be between 1 and " + 
+				ConsequencesSettings.MAX_NUM_STAGES);
+		}
+		
+		try {
+			// Get users details to fill in the statement parameters
+			PreparedStatement usrPstmt = SQLWrapper.getUser(conn, userID);
+			ResultSet usrRes = usrPstmt.executeQuery();
+			
+			// Look for first result
+			if (!usrRes.first()) {
+				// No result?  Throw an exception...
+				throw new HandlerException("Could not find a user with ID: " + userID);
+			}
+			
+			// Decide on whether to insert or update.  If on stage 1 then insert.
+			// Otherwise update.
+			if (stage == 1) {
+			}
+			else {
+			}
+			
+			// Close statements
+			usrRes.close();
+			usrPstmt.close(); 
+		}
+		catch(SQLException se) {
+			// Only catch SQL exceptions
+			errs.addException(this.getClass(), se);
+		}
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see com.totalchange.consequences.RequestHandler#data(char[], int, int)
 	 */
 	public void data(char[] ch, int start, int length)
 		throws HandlerException {
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see com.totalchange.consequences.RequestHandler#getChild(java.lang.String)
 	 */
 	public RequestHandler getChild(String name) throws HandlerException {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see com.totalchange.consequences.RequestHandler#end()
 	 */
 	public void end() throws HandlerException {
