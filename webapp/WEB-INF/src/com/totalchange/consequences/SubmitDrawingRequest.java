5c13dc63fcde81212228431289047a6bb2db6cf8
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-06-14 11:22:21 +0000

    Tested and working!

--- webapp/WEB-INF/src/com/totalchange/consequences/SubmitDrawingRequest.java
+++ webapp/WEB-INF/src/com/totalchange/consequences/SubmitDrawingRequest.java
@@ -17,7 +17,14 @@ import org.xml.sax.Attributes;
  */
 public class SubmitDrawingRequest implements RequestHandler {
 	
+	private Connection conn;
+	private ConsequencesErrors errs;
 	private PreparedStatement pstmt;
+	
+	private String drawingID = null;
+	private boolean complete = false; 
+	
+	private int paramDrawing = 0;
 
 	/**
 	 * @see com.totalchange.consequences.RequestHandler#start(com.totalchange.consequences.XMLWriter, java.sql.Connection, com.totalchange.consequences.ConsequencesErrors, org.xml.sax.Attributes)
@@ -29,11 +36,15 @@ public class SubmitDrawingRequest implements RequestHandler {
 		Attributes attributes)
 		throws HandlerException {
 		
+		// Get reference to connection and errors store
+		this.conn = conn;
+		this.errs = errs;
+		
 		// Make sure statement is null
 		pstmt = null;
 			
 		// Get required attributes
-		String drawingID = attributes.getValue(XMLConsts.AT_SUBMIT_DRAWING_DRAWING_ID);
+		drawingID = attributes.getValue(XMLConsts.AT_SUBMIT_DRAWING_DRAWING_ID);
 		if (drawingID == null) {
 			throw new HandlerException("Must pass a drawing id to submit a drawing");
 		}
@@ -49,6 +60,12 @@ public class SubmitDrawingRequest implements RequestHandler {
 		}
 		
 		String nextUserEmail = attributes.getValue(XMLConsts.AT_SUBMIT_DRAWING_NEXT_USER_EMAIL);
+		String distinguishedID = null;
+		
+		// If got a next user, will need to define a distiguished id
+		if (nextUserEmail != null) {
+			distinguishedID = new RandomGUID().toString();
+		}
 		
 		// Convert stage to an int.
 		int stage = Integer.valueOf(stageStr).intValue();
@@ -58,6 +75,9 @@ public class SubmitDrawingRequest implements RequestHandler {
 			throw new HandlerException("Stage value must be between 1 and " + 
 				ConsequencesSettings.MAX_NUM_STAGES);
 		}
+		
+		// See if completed or not 
+		complete = (stage >= ConsequencesSettings.MAX_NUM_STAGES);
 		
 		try {
 			// Get users details to fill in the statement parameters
@@ -73,8 +93,27 @@ public class SubmitDrawingRequest implements RequestHandler {
 			// Decide on whether to insert or update.  If on stage 1 then insert.
 			// Otherwise update.
 			if (stage == 1) {
+				// Insert a drawing
+				pstmt = SQLWrapper.insertDrawing(conn, drawingID, distinguishedID, userID,
+					usrRes.getString("name"), usrRes.getString("email"));
+					
+				// Copy across signature
+				SQLWrapper.copyClob(usrRes.getClob("signature"), pstmt, SQLWrapper.INS_DRAW_SIGNATURE);
+				
+				// Get parameter for inserting drawing
+				paramDrawing = SQLWrapper.INS_DRAW_DRAWING;
 			}
 			else {
+				// Update a drawing
+				pstmt = SQLWrapper.updateDrawing(conn, drawingID, complete, 
+					distinguishedID, stage, userID,	usrRes.getString("name"), 
+					usrRes.getString("email"));
+					
+				// Copy across signature
+				SQLWrapper.copyClob(usrRes.getClob("signature"), pstmt, SQLWrapper.UPD_DRAW_SIGNATURE);
+				
+				// Get parameter for updating drawing
+				paramDrawing = SQLWrapper.UPD_DRAW_DRAWING;
 			}
 			
 			// Close statements
@@ -98,13 +137,40 @@ public class SubmitDrawingRequest implements RequestHandler {
 	 * @see com.totalchange.consequences.RequestHandler#getChild(java.lang.String)
 	 */
 	public RequestHandler getChild(String name) throws HandlerException {
-		return null;
+		// Check for submission of drawing
+		if (name.equals(XMLConsts.EL_NEXT_DRAWING_DRAWING)) {
+			try {
+				return new ClobRequest(pstmt, paramDrawing);
+			}
+			catch (Exception e) {
+				throw new HandlerException(e.getMessage());
+			}
+		}
+		else {
+			return null;
+		}
 	}
 
 	/**
 	 * @see com.totalchange.consequences.RequestHandler#end()
 	 */
 	public void end() throws HandlerException {
+		try {
+			// Execute statement
+			pstmt.execute();
+		
+			// Close statement
+			pstmt.close();
+			
+			// See if this drawing is complete
+			if (complete) {
+				// Run the procedure to deal with a completed drawing...
+				CompleteDrawingProcessor.process(conn, drawingID);
+			}
+		}
+		catch (Exception e) {
+			errs.addException(this.getClass(), e);
+		}
 	}
 
 }
