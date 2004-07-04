3f510adf191692e8413ebbb115ec8f0cb049569b
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-07-04 22:48:51 +0000

    Had to completely simplify to cache the entire contents as a String to pass in one call to a set parameter to MySQL.  This is until can figure out a way to sensibly pass a stream of unknown length to MySQL.

--- webapp/WEB-INF/src/com/totalchange/consequences/ClobRequest.java
+++ webapp/WEB-INF/src/com/totalchange/consequences/ClobRequest.java
@@ -4,8 +4,6 @@
  */
 package com.totalchange.consequences;
 
-import java.io.PipedReader;
-import java.io.PipedWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -25,18 +23,12 @@ public class ClobRequest implements RequestHandler {
 	
 	private ConsequencesErrors errs;
 	
-	private PipedReader pipeIn;
-	private PipedWriter pipeOut;
-	private int totalLength;
+	private String clobStr = "";
 	
 	public ClobRequest(PreparedStatement pstmt, int paramNum) throws IOException {
 			
 		this.pstmt = pstmt;
 		this.paramNum = paramNum;
-		
-		// Setup piped reader/writer
-		pipeOut = new PipedWriter();
-		pipeIn = new PipedReader(pipeOut);
 	}
 
 	/**
@@ -58,13 +50,7 @@ public class ClobRequest implements RequestHandler {
 	public void data(char[] ch, int start, int length)
 		throws HandlerException {
 			
-		totalLength += length;
-		try {
-			pipeOut.write(ch, start, length);
-		}
-		catch (Exception e) {
-			errs.addException(this.getClass(), e);
-		}
+		clobStr += String.copyValueOf(ch, start, length);
 	}
 
 	/**
@@ -79,15 +65,7 @@ public class ClobRequest implements RequestHandler {
 	 */
 	public void end() throws HandlerException {
 		try {
-			// Flush output
-			pipeOut.flush();
-			
-			// Setup output on the prepared statement
-			pstmt.setCharacterStream(paramNum, pipeIn, totalLength);
-			
-			// Close streams
-			pipeOut.close();
-			pipeIn.close();
+			pstmt.setString(paramNum, clobStr);
 		}
 		catch (Exception e) {
 			errs.addException(this.getClass(), e);
