6e49be7b3b48bdc8f9e3ec9b5edef4c198d0c2bf
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2004-11-24 11:03:41 +0000

    Sorted out layout issues.

--- webapp/WEB-INF/src/com/totalchange/sketchingout/imageparsers/PdfImageParser.java
+++ webapp/WEB-INF/src/com/totalchange/sketchingout/imageparsers/PdfImageParser.java
@@ -8,10 +8,7 @@ import java.io.OutputStream;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
-import com.lowagie.text.Font;
-import com.lowagie.text.HeaderFooter;
 import com.lowagie.text.PageSize;
-import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfWriter;
@@ -26,8 +23,15 @@ public class PdfImageParser implements SketchingoutImageParser {
 	
 	private static final float MARGIN_LEFT = 50;
 	private static final float MARGIN_RIGHT = 50;
-	private static final float MARGIN_TOP = 50;
-	private static final float MARGIN_BOTTOM = 50;
+	private static final float MARGIN_TOP = 80;
+	private static final float MARGIN_BOTTOM = 80;
+	
+	private static final String TITLE_TEXT = "SKETCHING OUT";
+	private static final float TITLE_SPACE_FROM_TOP = 60;
+	private static final float TITLE_FONT_SIZE = 32;
+	
+	private static final float URL_SPACE_FROM_BOTTOM = 40;
+	private static final float URL_FONT_SIZE = 14;
 	
 	private static final float LINE_WIDTH = 1;
 	
@@ -35,31 +39,45 @@ public class PdfImageParser implements SketchingoutImageParser {
 	private PdfWriter writer;
 	private PdfContentByte cb;
 	
-	private float pageHeight = 0;
+	private float pageHeight, pageWidth = 0;
 	private float insetX, insetY, offsetX, offsetY = 0;
 	private float scale = 0;
 	
 	private void calculateDimensions(float pageWidth, float pageHeight, 
 			float drawingWidth, float drawingHeight) {
 		
-		// Need to keep page height as coords is bottom to top for Y axis
+		// Need to keep page height as coords system is bottom to top for Y axis
 		// instead of top to bottom
 		this.pageHeight = pageHeight;
+		this.pageWidth = pageWidth;
 		
 		// Get page dimensions taking into account margins
 		float pWidth = pageWidth - MARGIN_LEFT - MARGIN_RIGHT;
 		float pHeight = pageHeight - MARGIN_TOP - MARGIN_BOTTOM;
 		
-		// Need to scale the drawing to fill the available page
+		// Need to scale the drawing to fill the available page.  More often than
+		// not the height is the best bet to use.
 		scale = pHeight / drawingHeight;
 		
 		// Check the width doesn't overrun.  If so, need to base scale on
-		// the height.
+		// the width.
 		if ((drawingWidth * scale) > pWidth) scale = pWidth / drawingWidth; 
 		
 		// Now need to center the drawing by calculating the insets
-		insetX = (pWidth / 2) - ((drawingWidth * scale) / 2);
-		insetY = (pHeight / 2) - ((drawingHeight * scale) / 2);
+		insetX = ((pWidth / 2) - ((drawingWidth * scale) / 2)) + MARGIN_LEFT;
+		insetY = ((pHeight / 2) - ((drawingHeight * scale) / 2)) + MARGIN_TOP;
+		
+		System.out.println(
+			"P: " + pageWidth + ", " + pageHeight + " " +
+			"D: " + drawingWidth + ", " + drawingHeight + " " +
+			"I: " + insetX + ", " + insetY + " " +
+			"S: " + scale
+		);
+		
+		System.out.println(
+			"NP: " + pWidth + ", " + pHeight + " " +
+			"ND: " + (drawingWidth * scale) + ", " + (drawingHeight * scale)
+		);
 	}
 
 	/**
@@ -77,8 +95,10 @@ public class PdfImageParser implements SketchingoutImageParser {
 			document.addAuthor("Sketching Out");
 			document.addSubject("Sketching Out Drawing");
 			
+			// Embed the handwriting font
 			BaseFont baseFont = BaseFont.createFont(SKETCHING_OUT_FONT, BaseFont.WINANSI, BaseFont.EMBEDDED);
 			
+			/*
 			Font headerFont = new Font(baseFont, 24, Font.BOLD);
 			HeaderFooter header = new HeaderFooter(new Phrase("SKETCHING OUT", headerFont), false);
 			header.setAlignment(HeaderFooter.ALIGN_CENTER);
@@ -93,9 +113,31 @@ public class PdfImageParser implements SketchingoutImageParser {
 			HeaderFooter footer = new HeaderFooter(footerPhrase, false);
 			footer.setAlignment(HeaderFooter.ALIGN_CENTER);
 			document.setFooter(footer);
+			*/
 			
+			// Open the document and begin direct writing to it
 			document.open();
 			cb = writer.getDirectContent();
+			
+			// Define a title for the page
+			cb.beginText();
+			cb.setFontAndSize(baseFont, TITLE_FONT_SIZE);
+			cb.showTextAligned(PdfContentByte.ALIGN_CENTER, TITLE_TEXT, 
+					pageWidth / 2, pageHeight - TITLE_SPACE_FROM_TOP, 0);
+			cb.endText();
+			
+			// Define a footer for the page
+			cb.beginText();
+			cb.setFontAndSize(baseFont, URL_FONT_SIZE);
+			cb.showTextAligned(PdfContentByte.ALIGN_CENTER, 
+					SketchingoutSettings.URL_ROOT, pageWidth / 2, URL_SPACE_FROM_BOTTOM,
+					0);
+			cb.endText();
+			
+			// Put an action over the footer to link to the website
+			
+			
+			// Define the line width for the entire drawing
 			cb.setLineWidth(LINE_WIDTH);
 		}
 		catch (DocumentException de) {
@@ -119,8 +161,8 @@ public class PdfImageParser implements SketchingoutImageParser {
 	 */
 	public void startCanvas(int x, int y, int width, int height)
 			throws SketchingoutImageParserException {
-		offsetX = insetX + x;
-		offsetY = insetY + y;
+		offsetX = x;
+		offsetY = y;
 	}
 
 	/**
@@ -134,9 +176,21 @@ public class PdfImageParser implements SketchingoutImageParser {
 	 */
 	public void moveTo(double x, double y)
 			throws SketchingoutImageParserException {
+		
+		// This might take some explaining.  It's the same for the line to, so
+		// may as well go in depth here.
+		// The X axis needs to be the canvas offset + the width, then scaled to
+		// fill the available page space.  The inset value is then added after the
+		// coord has been scaled.  This is because the inset uses the pages coord
+		// environment, and the scaling brings the drawing into the pages coord
+		// environment.
+		//
+		// The Y axis does the same, but it's also inverted.  This is because the
+		// PDF pages Y coord runs from bottom to top, but Sketching Outs drawing
+		// Y coord runs from top to bottom. 
 		cb.moveTo(
-			((float) x + offsetX) * scale, 
-			pageHeight - (((float) y + offsetY) * scale)
+			(((float) x + offsetX) * scale) + insetX, 
+			pageHeight - ((((float) y + offsetY) * scale) + insetY)
 		);
 	}
 
@@ -145,9 +199,11 @@ public class PdfImageParser implements SketchingoutImageParser {
 	 */
 	public void lineTo(double x, double y)
 			throws SketchingoutImageParserException {
+		
+		// See moveTo for explanation.
 		cb.lineTo(
-			((float) x + offsetX) * scale, 
-			pageHeight - (((float) y + offsetY) * scale)
+			(((float) x + offsetX) * scale) + insetX, 
+			pageHeight - ((((float) y + offsetY) * scale) + insetY)
 		);
 	}
 
