4072991ced665af9c276c8fc6f27d6bd4acb046b
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2005-01-15 01:05:14 +0000

    Put in a few test email addresses

--- webapp/WEB-INF/src/com/totalchange/sketchingout/SketchingoutEmails.java
+++ webapp/WEB-INF/src/com/totalchange/sketchingout/SketchingoutEmails.java
@@ -11,22 +11,35 @@ package com.totalchange.sketchingout;
 public class SketchingoutEmails {
 	public static final String SUBST_NAME = ":name:";
 	public static final String SUBST_EMAIL = ":email:";
+	public static final String SUBST_BODY_PART = ":body_part:";
+	public static final String SUBST_A_OR_SOME = ":a_or_some:";
+	public static final String SUBST_THOSE_ARE_OR_THAT_IS = ":those_are_or_that_is:";
 	
+	public static final int EMAILS_FROM_NAME = 0;
+	public static final int EMAILS_FROM_EMAIL = 1;
+	public static final int EMAILS_SUBJECT = 2;
+	public static final int EMAILS_BODY = 3;
 	public static final String[][] EMAILS = {
 		{
 			"Bob Geldof",
-			"bob@geldof.com",
+			"bob.geldof@sketchingout.co.uk",
 			"Some email subject",
 			"Hi " + SUBST_NAME + ",\n\n" +
-			"Please find attached your sketching out pictures.\n\n" +
+			"Please find attached your sketching out picture.  The starving children of Africa love it more than stale bread and rancid water.\n\n" +
 			"Kind regards,\n\n" +
 			"Bob"
 		}, 
 		{
-			"",
-			"",
-			"",
-			""
+			"Test Person",
+			"test@sketchingout.co.uk",
+			"Test Subject",
+			"Test Body"
+		},
+		{
+			"Test Person 2",
+			"test2@sketchingout.co.uk",
+			"Test Subject to " + SUBST_NAME + " for drawing " + SUBST_BODY_PART,
+			"Test multiple: " + SUBST_NAME + ", " + SUBST_NAME + ", " + SUBST_NAME
 		}
 	}; 
 }
