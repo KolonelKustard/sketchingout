2caf112a783a5f48b3cce9f5758399580806e4bb
simon <simon@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2005-03-07 21:39:05 +0000

    added constant subject string and changed friends email

--- webapp/WEB-INF/src/com/totalchange/sketchingout/SketchingoutEmails.java
+++ webapp/WEB-INF/src/com/totalchange/sketchingout/SketchingoutEmails.java
@@ -17,6 +17,8 @@ public class SketchingoutEmails {
 	public static final String SUBST_A_OR_SOME = ":a_or_some:";
 	public static final String SUBST_THOSE_ARE_OR_THAT_IS = ":those_are_or_that_is:";
 	public static final String SUBST_URL = ":url:";
+
+	public static final String CONST_SUBJECT = "Here's your drawing from SketchingOut.co.uk";
 	
 	public static final int EMAILS_FROM_NAME = SketchingoutEmail.EMAILS_ARRAY_FROM_NAME;
 	public static final int EMAILS_FROM_EMAIL = SketchingoutEmail.EMAILS_ARRAY_FROM_EMAIL;
@@ -26,7 +28,7 @@ public class SketchingoutEmails {
 		{
 			"Bobby McFerrin",
 			"bobbymcferrin@sketchingout.co.uk",
-			"SketchingOut Strikes Again",
+			CONST_SUBJECT,
 			"Hi " + SUBST_TO_NAME + ",\n\n" +
 			"The drawing you did on SketchingOut is completed! We think your " + SUBST_BODY_PART + " makes the drawing...!\n\n" +
 			"Kind regards,\n\n" +
@@ -35,7 +37,7 @@ public class SketchingoutEmails {
 		{
 			"Jimmy Jim",
 			"jimmyjim@sketchingout.co.uk",
-			"A sketchingOut production",
+			CONST_SUBJECT,
 			"What on earth is this? its your completed drawing from SketchingOut. Thats what.\n\n" +
 			"Best washes,\n\n" +
 			"jim"
@@ -43,7 +45,7 @@ public class SketchingoutEmails {
 		{
 			"Wailing Tuff",
 			"wailingtuff@sketchingout.co.uk",
-			"Here me now, its SketchingOut so it is",
+			CONST_SUBJECT,
 			"Here's your drawing. Remember? SketchingOut? oh well, just have a look anyway.\n\n" +
 			"Love,\n\n" +
 			"Wailing T"
@@ -51,7 +53,7 @@ public class SketchingoutEmails {
 		{
 			"Devious Honey R. Smooth",
 			"devious@sketchingout.co.uk",
-			"Are you down with this?",
+			CONST_SUBJECT,
 			"You have Won...! ...A drawing. Enjoy your prize.\n\n" +
 			"All the best,\n\n" +
 			"Mr. Smooth"
@@ -59,7 +61,7 @@ public class SketchingoutEmails {
 		{
 			"Stealth Maestro leech Loco",
 			"stealth@sketchingout.co.uk",
-			"SketchingOut is commin' at ya!",
+			CONST_SUBJECT,
 			"Crikey, look at the state of this! We think its ace. well done.\n\n" +
 			"Peace,\n\n" +
 			"S. Loco"
@@ -67,7 +69,7 @@ public class SketchingoutEmails {
 		{
 			"D. Magical Robinson Clinton",
 			"d_mag@sketchingout.co.uk",
-			"Word. SketchingOut says safety first",
+			CONST_SUBJECT,
 			"Have you seen this? well have a look, its your finished drawing!\n\n" +
 			"Massive respect,\n\n" +
 			"Mr. Magic"
@@ -75,7 +77,7 @@ public class SketchingoutEmails {
 		{
 			"Master Fly Beazer Slick",
 			"fly@sketchingout.co.uk",
-			"SketchingOut is big in the area",
+			CONST_SUBJECT,
 			"Does this look normal to you? " +SUBST_THOSE_ARE_OR_THAT_IS+ " the strangest " + SUBST_BODY_PART + " We've ever seen!\n\n" +
 			"Shhhh,\n\n" +
 			"Fly"
@@ -83,7 +85,7 @@ public class SketchingoutEmails {
 		{
 			"Mack P. Bobby Kicks",
 			"mack@sketchingout.co.uk",
-			"This isn't junk, its SketchingOut!",
+			CONST_SUBJECT,
 			"Hey, your drawing is done! lets take a look, arrgghhh! it's hideous!\n\n" +
 			"Check yourself,\n\n" +
 			"Mack"
@@ -91,7 +93,7 @@ public class SketchingoutEmails {
 		{
 			"Adam Ant",
 			"adamant@sketchingout.co.uk",
-			"Stand and Deliver! its SketchingOut!",
+			CONST_SUBJECT,
 			"Is this not the best picture you've ever seen?!\n\n" +
 			"Kind regards,\n\n" +
 			"The Dandy Highwayman"
@@ -99,7 +101,7 @@ public class SketchingoutEmails {
 		{
 			"Shakin' Stevens",
 			"shakey@sketchingout.co.uk",
-			"News of This Old House (and a SketchingOut picture)",
+			CONST_SUBJECT,
 			"I lied about the old house, its rubbish, unlike this picture!\n\n" +
 			"Keep on Rockin',\n\n" +
 			"Shakey"
@@ -107,7 +109,7 @@ public class SketchingoutEmails {
 		{
 			"Your Mum",
 			"mumsie@sketchingout.co.uk",
-			"What time do you call this?",
+			CONST_SUBJECT,
 			"Hey that looks like my mother! how dare you!\n\n" +
 			"Tsk,\n\n" +
 			"Mum"
@@ -115,7 +117,7 @@ public class SketchingoutEmails {
 		{
 			"A Wellwisher",
 			"wellwisher@sketchingout.co.uk",
-			"I'm very proud of you",
+			CONST_SUBJECT,
 			"This picture would have been fantastic. If you hadn't drawn " + SUBST_A_OR_SOME + " stupid " + SUBST_BODY_PART + "!\n\n" +
 			"Wishing you well,\n\n" +
 			"From nobody in particular"
@@ -123,7 +125,7 @@ public class SketchingoutEmails {
 		{
 			"E.T.",
 			"et@sketchingout.co.uk",
-			"SketchingOut phone home",
+			CONST_SUBJECT,
 			"Your picture is ready! I bet the other drawers are glad you were involved...\n\n" +
 			"Stay tuned,\n\n" +
 			"E.T"
@@ -131,7 +133,7 @@ public class SketchingoutEmails {
 		{
 			"Boney M",
 			"boneym@sketchingout.co.uk",
-			"Another SketchingOut exclusive",
+			CONST_SUBJECT,
 			"Wow, what an ace piece of drawing teamwork!\n\n" +
 			"You've made my day,\n\n" +
 			"Boney"
@@ -139,7 +141,7 @@ public class SketchingoutEmails {
 		{
 			"David Bellamy",
 			"daveb@sketchingout.co.uk",
-			"I love SketchingOut and mucky moss",
+			CONST_SUBJECT,
 			"Here's your finished drawing. Make of it what you will.\n\n" +
 			"Lovely,\n\n" +
 			"Mr. B"
@@ -147,7 +149,7 @@ public class SketchingoutEmails {
 		{
 			"Rolf Harris",
 			"daveb@sketchingout.co.uk",
-			"SketchingOut is taking my job",
+			CONST_SUBJECT,
 			"Can you guess what this is yet? No, me neither...!\n\n" +
 			"G'Day,\n\n" +
 			"Rolf"
@@ -155,7 +157,7 @@ public class SketchingoutEmails {
 		{
 			"Bobby Bones",
 			"bb@sketchingout.co.uk",
-			"Last night SketchingOut saved my life",
+			CONST_SUBJECT,
 			"Here's your drawing. We hope you like it. If you don't, we really are very sorry.\n\n" +
 			"Hope you get better soon,\n\n" +
 			"Bob"
@@ -163,7 +165,7 @@ public class SketchingoutEmails {
 		{
 			"Denzil",
 			"denzil@sketchingout.co.uk",
-			"oo bee ee? oh Sketchingout",
+			CONST_SUBJECT,
 			"Here's your drawing. Wasn't it worth the wait?!\n\n" +
 			"I think so,\n\n" +
 			"Denzil"
@@ -171,7 +173,7 @@ public class SketchingoutEmails {
 		{
 			"Frankie",
 			"frankie@sketchingout.co.uk",
-			"Frankie says, choose SketchingOut",
+			CONST_SUBJECT,
 			"Here's your completed drawing. My 2 year old sister couldn't have done any better...\n\n" +
 			"Relax, don't do it,\n\n" +
 			"Frank"
@@ -180,13 +182,13 @@ public class SketchingoutEmails {
 	
 	public static final String[][] EMAILS_TO_FRIENDS = {
 		{
-			"Bobby McFerrin",
-			"bobbymcferrin@sketchingout.co.uk",
-			"SketchingOut Strikes Again",
+			"SketchingOut",
+			"whoareyou@sketchingout.co.uk",
+			"A SketchingOut.co.uk drawing request",
 			"Hi " + SUBST_TO_NAME + ",\n\n" +
-			"The drawing you did on SketchingOut is completed! We think your " + SUBST_BODY_PART + " makes the drawing...!\n\n" +
+			"Your Pal, " + SUBST_FROM_NAME + " " + SUBST_FROM_EMAIL + "has invited you to draw a picture!" + "\n" +
 			"Kind regards,\n\n" +
-			"Bob"
+			"www.SketchingOut.co.uk"
 		}
 	};
 }
