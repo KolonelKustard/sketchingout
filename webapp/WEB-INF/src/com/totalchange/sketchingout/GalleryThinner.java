23fda4f68d0fd8c16ffae3f45d5a5b9ee14e0c10
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2005-10-30 16:54:41 +0000

    Added the code that does the deletion

--- webapp/WEB-INF/src/com/totalchange/sketchingout/GalleryThinner.java
+++ webapp/WEB-INF/src/com/totalchange/sketchingout/GalleryThinner.java
@@ -14,7 +14,13 @@ public class GalleryThinner {
 		catch (Exception e) {}
 	}
 	
-	private static void DeleteDrawing(ResultSet res) throws SQLException {
+	/**
+	 * Deletes the files associated with a drawing
+	 * 
+	 * @param res
+	 * @throws SQLException
+	 */
+	private static void deleteDrawingFiles(ResultSet res) throws SQLException {
 		String thumb = res.getString("thumbnail_filename");
 		if (thumb != null)
 			DeleteDrawing(new File(SketchingoutSettings.FS_DRAWING_STORE + thumb));
@@ -28,7 +34,49 @@ public class GalleryThinner {
 			DeleteDrawing(new File(SketchingoutSettings.FS_DRAWING_STORE + swf));
 	}
 	
-	public static void DeleteDrawing(PreparedStatement pstmt) {
+	/**
+	 * Deletes the last gallery drawing from the gallery
+	 * 
+	 * @param conn
+	 */
+	private static void deleteLastDrawing(Connection conn) throws SQLException {
+		// Find the oldest drawing in the gallery
+		PreparedStatement pstmt = SQLWrapper.getOldestGalleryDrawing(conn);
+		ResultSet res = pstmt.executeQuery();
+		try {
+			if (res.first()) {
+				// Delete all the files associated with this drawing
+				deleteDrawingFiles(res);
+				
+				// Now delete the gallery entry
+				SQLWrapper.deleteDrawing(conn, res.getString("id"));
+			}
+		}
+		finally {
+			res.close();
+			pstmt.close();
+		}
+	}
+	
+	/**
+	 * Deletes old drawings from the gallery based upon the defined maximum
+	 * number of gallery items.
+	 * 
+	 * @param conn
+	 * @throws SQLException
+	 */
+	public static void deleteOldDrawings(Connection conn) throws SQLException {
+		// Only bother if the gallery is restricted
+		if (SketchingoutSettings.MAX_STORED_DRAWINGS == -1) return;
 		
+		// Now need to find out how many gallery items there are...
+		int numItems = SQLWrapper.getGallerySize(conn);
+		
+		// Find the difference between the max size and the current size
+		int diff = numItems - SketchingoutSettings.MAX_STORED_DRAWINGS;
+		
+		// Delete the difference
+		for (int num = 0; num < diff; num++)
+			deleteLastDrawing(conn);
 	}
 }
