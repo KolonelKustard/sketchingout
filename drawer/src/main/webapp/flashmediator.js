46af86604ac8a5d2e6212ead326fde327754a84a
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2007-01-31 09:56:16 +0000

    Fixed up some script for file upload handling

--- drawer/src/main/webapp/flashmediator.js
+++ drawer/src/main/webapp/flashmediator.js
@@ -3,18 +3,19 @@ function FlashMediator(form, flashProxy, flashMethod) {
 	this.flashProxy = flashProxy;
 	this.flashMethod = flashMethod;
 	
-	// Add a hidden form field to the form
+	// Make up an id
+	this.id = Math.round(Math.random() * new Date().getTime());
 	
+	// Set the hidden field of the form to the id
+	this.form.MNNNnnnnggggghhhhhh.value = this.id;
 	
 	// Link the form submission to our handler
 	this.form.onsubmit = this.submitHandler;
 }
 
 FlashMediator.prototype.submitHandler = function() {
-	alert("Test");
-	
 	// Alert the flash movie to the submission
-	this.flashProxy.call(this.flashMethod, "bum");
+	this.flashProxy.call(this.flashMethod, this.id);
 	
 	return true;
 }
