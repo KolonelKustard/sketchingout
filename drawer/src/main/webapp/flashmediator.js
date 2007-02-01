14a1fecbbae8fd890dc081f41d70d924d4244691
kolonel <kolonel@14c86af3-cb12-0410-9cd8-d2bb3d1c3e0d>
2007-02-01 22:08:16 +0000

    Added support for background images and loading/saving a drawing

--- drawer/src/main/webapp/flashmediator.js
+++ drawer/src/main/webapp/flashmediator.js
@@ -1,21 +1,17 @@
-function FlashMediator(form, flashProxy, flashMethod) {
-	this.form = form;
-	this.flashProxy = flashProxy;
-	this.flashMethod = flashMethod;
-	
+function flashMediator(form, flashProxy, flashMethod) {
 	// Make up an id
-	this.id = Math.round(Math.random() * new Date().getTime());
+	var id = Math.round(Math.random() * new Date().getTime());
 	
 	// Set the hidden field of the form to the id
-	this.form.MNNNnnnnggggghhhhhh.value = this.id;
+	form.MNNNnnnnggggghhhhhh.value = id;
 	
 	// Link the form submission to our handler
-	this.form.onsubmit = this.submitHandler;
-}
-
-FlashMediator.prototype.submitHandler = function() {
-	// Alert the flash movie to the submission
-	this.flashProxy.call(this.flashMethod, this.id);
+	form.onsubmit = function() {
+		alert("TART " + flashMethod + " [" + id + "]");
 	
-	return true;
+		// Alert the flash movie to the submission
+		flashProxy.call(flashMethod, id);
+		
+		return true;
+	}
 }
